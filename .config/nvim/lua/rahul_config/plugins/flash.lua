d7ae0050b035ba557dec426d4181f179db54f0ae
Rahul Desar <desarrahul@gmail.com>
2025-03-27 02:57:01 +0545

    checkpoint: migrating to v0.11 neovim

--- .config/nvim/lua/rahul_config/plugins/flash.lua
+++ .config/nvim/lua/rahul_config/plugins/flash.lua
@@ -3,13 +3,27 @@ return {
 	event = "VeryLazy",
 	---@type Flash.Config
 	opts = {},
-  -- stylua: ignore
-  keys = {
-    -- TODO : map keys without conflict
-    { "<c-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	keys = {
+		-- TODO : map keys without conflict
+		{
+			"<c-s>",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		-- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+		-- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	},
+	-- stylua: ignore
 }
